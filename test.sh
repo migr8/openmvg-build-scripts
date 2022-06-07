171de6c8fa3f1206f16c8e1d13c483e25b501e53
Daniel 'dbeef' Zalega <daniel.zalega@gmail.com>
2022-06-07 09:36:10 +0200

    OMP - forcing 1 thread

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+source common.sh
+
+# OMP is explicitly disabled when building both Ceres and OpenMVG,
+# but just to be extra paranoid, also force one thread here:
+export OMP_NUM_THREADS=1
+
 $(find build/ -type f -executable | grep openMVG_test_sift)
+
 echo -e "\\n\\nImage hashes & sizes:"
 md5sum gaussian_octave_0_*.png && ls -all gaussian_octave_0_*.png  
 
